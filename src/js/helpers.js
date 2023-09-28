c8de077f40275b002dffebdd0f5b53d43a22b3bd
AmauriBlanco <blancoamauri@gmail.com>
2023-09-28 16:44:17 -0300

    5-1 Criando a conversão monetária

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -3,11 +3,19 @@ function obterElementoId(nomeCampo) {
 }
 
 function inserirValores(elemento, valor){
-    return obterElementoId(elemento).innerText = `${valor}`
+    return obterElementoId(elemento).innerText = `${converterMoeda(valor)}`
 }
 
 function inserirQuantidades(elemento, valor, unidade = '') {
     obterElementoId(elemento).innerText = `${valor}${unidade}`
+}
+
+function converterMoeda(valor){
+    const valorConvertido = new Intl.NumberFormat('pt-BR', {
+        currency: 'BRL',
+        style: 'currency'
+    }).format(valor)
+    return valorConvertido
 }
 
 export {inserirValores, inserirQuantidades}
