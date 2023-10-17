ff54b7fec96c2a383a1e68fcd3e47cbe37e60c8b
AmauriBlanco <blancoamauri@gmail.com>
2023-10-17 15:11:05 -0300

    5-2 Criando máscara monetária

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
 import { calcularBebida } from "./bebidas.js";
 import { calcularComida } from "./comida.js";
 import { obterFormulario, mostrarBebida, mostrarComida } from "./dom.js";
+import { formatarMoeda } from "./mascara.js";
 
 const informacoesDeEntrada = obterFormulario();
+
+formatarMoeda(informacoesDeEntrada);
 
 function bebida() {
     let valores = {
